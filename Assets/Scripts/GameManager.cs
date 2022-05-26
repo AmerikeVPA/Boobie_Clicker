9b55838a1a426ec9c5509a74c3f8f75ce0e300f1
Alzound <aaxlcd@gmail.com>
2022-05-26 09:58:10 -0500

    Arreglar eventos

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,22 +9,23 @@ public class GameManager : MonoBehaviour
     public TextMeshProUGUI moneyTxt; 
     public GameObject[] positionT; 
     int money, randNum = 0;
-    protected float time; 
+    protected float time, timeEvent; 
     protected int oneSecond = 1;
+    protected int num = 1;
+    public GameObject boobs; 
     public AudioSource audioS;
     public AudioClip clipCoin;   
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
         time += Time.deltaTime; 
-
+        timeEvent += Time.deltaTime;  
+        if(timeEvent > 25)
+        {
+            StartCoroutine(RandomEvent());
+            time = 0; 
+        }
         //Esta función aumenta el valor * segundo del dinero. 
         PlusQuantity(); 
         //Esta función es para que al tocar una vez aumente más rápido el dinero. 
@@ -49,19 +50,51 @@ public class GameManager : MonoBehaviour
         {
    	        // Casts the ray and get the first game object hit
    	        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
-   	        if(hit.collider.gameObject.CompareTag("Boobs"))
+   	        if(hit.collider.gameObject.CompareTag("Boobs") && num == 1)
             {
                 money++;
+                audioS.PlayOneShot(clipCoin);
+            }
+            if(hit.collider.gameObject.CompareTag("Boobs") && num == 2)
+            {
+                money += 2;
                 audioS.PlayOneShot(clipCoin);
             }
         }
     }
 
-/*
-    public void Event_Teleport()
+    public void Event1()
     {
-        positionT[randNum] = Random.Range(0,7);
-         
+        if(money >= 300)
+        {
+
+        }
     }
-    */
+
+    public void Event2()
+    {
+        if(money >= 600)
+        {
+            
+        }
+    }
+
+     public IEnumerator RandomEvent()
+    {
+        int i = 0; 
+        do
+        {
+            Debug.Log("Entro"); 
+            randNum = Random.Range(0, 9);  
+            boobs.transform.position = positionT[randNum].transform.position;
+            yield return new WaitForSeconds(7);
+            i++;   
+        }while(i > 6);
+        
+    }
 }
+
+   
+
+    
+
