764a4f19966bd24803bb701e36e8dd092c17e760
VicPina <vicpanzu7@gmail.com>
2022-06-03 07:26:33 -0500

    Minigame instruction comp

--- Assets/Scripts/MinigameManager.cs
+++ Assets/Scripts/MinigameManager.cs
@@ -11,21 +11,27 @@ public class MinigameManager : MonoBehaviour
 
     public void MoveImages(GameObject imageToMove)
     {
-        Vector3 endV3 = new Vector3(imageToMove.transform.localPosition.x - 500f, imageToMove.transform.localPosition.y, imageToMove.transform.localPosition.z);
+        Vector3 endV3 = new Vector3(imageToMove.transform.localPosition.x - 650f, imageToMove.transform.localPosition.y, imageToMove.transform.localPosition.z);
         StartCoroutine(MoveGraphic(imageToMove, endV3));
     }
     IEnumerator MoveGraphic(GameObject imageToMove, Vector3 endPos)
     {
-        yield return new WaitForSeconds(0.1f);
-        imageToMove.transform.localPosition = Vector3.MoveTowards(imageToMove.transform.localPosition, endPos, 37.5f);
-        StartCoroutine(MoveGraphic(imageToMove, endPos));
+        yield return new WaitForSeconds(0.03f);
+        if (imageToMove.transform.localPosition != endPos)
+        {
+            imageToMove.transform.localPosition = Vector3.MoveTowards(imageToMove.transform.localPosition, endPos, 37.5f);
+            StartCoroutine(MoveGraphic(imageToMove, endPos));
+        }
     }
     public void FadeOut(Graphic affctdGrphc)
     {
         affctdGrphc.CrossFadeAlpha(0, .5f, false);
         Destroy(affctdGrphc.gameObject, 1.25f);
     }
-    public void FadeIn(Graphic affctdGrphc) { affctdGrphc.CrossFadeAlpha(255, 1.5f, false); }
+    public void FadeIn(Graphic affctdGrphc) 
+    {
+        affctdGrphc.gameObject.SetActive(true);
+        affctdGrphc.CrossFadeAlpha(255, 1.5f, false); }
     public void DestroyObj(GameObject affctdObj) { Destroy(affctdObj, 1.5f); }
 
 }
